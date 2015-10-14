384981c342f29e7dc9d04bc304c4d234668a8274
kushkp <ankushp89@gmail.com>
2015-10-14 15:46:59 -0700

    solve digital root iteratively

--- d1.rb
+++ d1.rb
@@ -1,10 +1,38 @@
-def digital_root(num)
+def digital_rootr(num)
   return num if num < 10
 
   tens = num / 10
   ones = num % 10
 
-  digital_root(tens) + ones
+  digital_rootr(tens + ones)
 end
 
-p digital_root(116)
+def digital_rooti(num)
+  res = num
+  while res > 9
+    res = digital_step(res)
+  end
+
+  return res
+end
+
+def digital_step(num)
+  res = 0
+
+  while num / 10 > 0
+    res += num % 10
+    num /= 10
+  end
+
+  res += num
+
+  return res
+end
+
+def caesar_cipher
+end
+
+#tests
+
+  p digital_rootr(166)
+  p digital_rooti(166)
