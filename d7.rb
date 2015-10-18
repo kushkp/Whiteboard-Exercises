d17e458b6b5f9eeffb7b79852bad9b37c210a010
kushkp <ankushp89@gmail.com>
2015-10-17 23:00:20 -0700

    fix move_zeros

--- d7.rb
+++ d7.rb
@@ -4,7 +4,7 @@ def move_zeros(array)
   z = arr.length - 1
   i = 0
 
-  while i < arr.length || i == z
+  while i < arr.length && i < z
     if arr[i] == 0
       arr[i], arr[z] = arr[z], arr[i]
       z -= 1
@@ -13,11 +13,5 @@ def move_zeros(array)
     end
   end
 
-
+  arr
 end
-
-# tests
-p move_zeros([2,3,4,5,6])
-p move_zeros([0,0,0,0,0])
-p move_zeros([2,0,4,6,0,0,4])
-p move_zeros([2,0,4,6,0,0])
