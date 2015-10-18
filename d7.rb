ab0e459f8121235608616578ba3e8d7f7cea5eb3
kushkp <ankushp89@gmail.com>
2015-10-17 23:15:32 -0700

    finish look_and_say

--- d7.rb
+++ d7.rb
@@ -15,3 +15,39 @@ def move_zeros(array)
 
   arr
 end
+
+def look_and_say(arr)
+  return [] if arr.empty?
+
+  result = []
+  i = 1
+  current_val = arr[0]
+  count = 1
+
+  while i < arr.length
+    if arr[i] == current_val
+      count += 1
+    else
+      result << [count, current_val]
+      current_val = arr[i]
+      count = 1
+    end
+
+    i += 1
+  end
+
+  result << [count, current_val]
+  result
+end
+
+# tests
+# p move_zeros([2,3,4,5,6])
+# p move_zeros([0,0,0,0,0])
+# p move_zeros([2,0,4,6,0,0,4])
+# p move_zeros([2,0,4,6,0,0])
+# p move_zeros([0,0,4,6,0,0])
+
+p look_and_say([1])
+p look_and_say([1,1])
+p look_and_say([2,1])
+p look_and_say([1,2,1,1])
