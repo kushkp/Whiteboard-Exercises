d4d773983a91338ba0cf88becd4b0bac4234393d
kushkp <ankushp89@gmail.com>
2015-10-19 22:08:03 -0700

    setup loops

---
+++ extra.rb
@@ -0,0 +1,13 @@
+# Given number represented in an array (123 = [1,2,3]).
+# Write a function that increments number by one.
+def increment_num(digits)
+  i = digits.length - 1
+  carry = 0
+
+  while i >= 0
+    if 
+
+    i -= 1
+  end
+
+end
